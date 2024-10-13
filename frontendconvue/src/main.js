89f5b16de980ea1d1a38e9575edbdcf93c6af2d6
liz <liz.yanez.lobe@gmail.com>
2024-10-13 18:40:23 -0300

    firebase

--- frontendconvue/src/main.js
+++ frontendconvue/src/main.js
@@ -2,10 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import './firebase';
 
 const app = createApp(App);
 
-store.dispatch('observarUsuario');
-
-app.use(router).use(store).mount('#app');
+app.use(router);
+app.use(store);
+app.mount('#app');
